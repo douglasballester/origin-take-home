2d96ce8f3d52157f8e0c0eab36e7eb92abd9f6f4
Victor Eduardo Scherer <victorescherer@hotmail.com>
2021-09-03 17:54:54 -0300

    WIP Validators

---
+++ Origin.Application/Validators/HouseValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Origin.Application.Dtos.Insurace;
+
+namespace Origin.Application.Validators
+{
+    public class HouseValidator : AbstractValidator<HouseDTO>
+    {
+        public HouseValidator()
+        {
+
+        }
+    }
+}
---
+++ Origin.Application/Validators/VehicleValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+using Origin.Application.Dtos.Insurace;
+
+namespace Origin.Application.Validators
+{
+    public class VehicleValidator : AbstractValidator<VehicleDTO>
+    {
+    }
+}
